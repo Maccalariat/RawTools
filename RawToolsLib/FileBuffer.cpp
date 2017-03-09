9fff5ee527cb181fd4a1f51d6935c5b55d5d5cb3
hans <hans@linux-6zbh.niebelheim>
2017-03-10 09:11:54 +1100

    workstation sync

--- RawToolsLib/FileBuffer.cpp
+++ RawToolsLib/FileBuffer.cpp
@@ -1,87 +1,99 @@
 #include "FileBuffer.h"
 
+FileBuffer::FileBuffer(ProcessBlock &processBlock)
+{
 
-FileBuffer::FileBuffer(ProcessBlock& processBlock) {
+    std::ifstream inputFile;
 
-	std::ifstream inputFile;
-
-	// _processBlock = processBlock;
-	inputFile.open(processBlock.filename, std::ios::binary | std::ios::in);
-	if (inputFile.bad()) {
-		processBlock.statusCode = processBlock.OK;
-		processBlock.statusMessage.assign("problem opening the file. Terminal");
-		inputFile.close();
-		return;
-		}
-	inputFile.seekg(0);
-	std::streampos begin = inputFile.tellg();
-	inputFile.seekg(0, std::ios::end);
-	std::streampos end = inputFile.tellg();
-
-	if (end) {
-		inputFile.seekg(0);
-		memoryFile.resize(end);
-		inputFile.read((char *) &memoryFile[0], end);
-		}
-	else {
-		processBlock.statusCode = processBlock.BadFileName;
-		processBlock.statusMessage.assign("the file has zero length. Terminal");
-		inputFile.close();
-		return;
-		}
-
+    // _processBlock = processBlock;
+    inputFile.open(processBlock.filename, std::ios::binary | std::ios::in);
+    if (inputFile.bad())
+    {
+	processBlock.statusCode = processBlock.OK;
+	processBlock.statusMessage.assign("problem opening the file. Terminal");
 	inputFile.close();
-	processBlock.bigEndian = getEndian();
-	bigEndian = processBlock.bigEndian;
-	}
+	return;
+    }
+    inputFile.seekg(0);
+    std::streampos begin = inputFile.tellg();
+    inputFile.seekg(0, std::ios::end);
+    std::streampos end = inputFile.tellg();
 
+    if (end)
+    {
+	inputFile.seekg(0);
+	memoryFile.resize(end);
+	inputFile.read((char *)&memoryFile[0], end);
+    }
+    else
+    {
+	processBlock.statusCode = processBlock.BadFileName;
+	processBlock.statusMessage.assign("the file has zero length. Terminal");
+	inputFile.close();
+	return;
+    }
 
-FileBuffer::~FileBuffer() {
-	}
+    inputFile.close();
+    processBlock.bigEndian = getEndian();
+    bigEndian = processBlock.bigEndian;
+}
 
-bool FileBuffer::getEndian() {
+FileBuffer::~FileBuffer()
+{
+}
 
-	uint16_t endian = (memoryFile[0] << 8) | memoryFile[1];
+bool FileBuffer::getEndian()
+{
 
-	if (endian == 0x4949) { // 'II'
-		return false;
-		}
-	else {
-		if (endian == 0x4D4D) { // 'MM'
-			return true;
-			}
-		}
+    uint16_t endian = (memoryFile[0] << 8) | memoryFile[1];
+
+    if (endian == 0x4949)
+    { // 'II'
 	return false;
+    }
+    else
+    {
+	if (endian == 0x4D4D)
+	{ // 'MM'
+	    return true;
 	}
-
+    }
+    return false;
+}
 
 // given an offset, return the endian-correct unit16 value
-uint16_t FileBuffer::getUint16(const size_t offset) {
-	if (bigEndian) {
-		return ((memoryFile[offset] << 8) | memoryFile[offset + 1]);
-		}
-	else {
-		return ((memoryFile[offset + 1] << 8) | memoryFile[offset]);
-		}
-	}
+uint16_t FileBuffer::getUint16(const size_t offset)
+{
+    if (bigEndian)
+    {
+	return ((memoryFile[offset] << 8) | memoryFile[offset + 1]);
+    }
+    else
+    {
+	return ((memoryFile[offset + 1] << 8) | memoryFile[offset]);
+    }
+}
 
 // given an offset, return the endian-correct unit32 value
-uint32_t FileBuffer::getUint32(const size_t offset) {
-	uint32_t fValue = (memoryFile[offset] << 24 |
-					   memoryFile[offset + 1] << 16 |
-					   memoryFile[offset + 2] << 8) |
-		memoryFile[offset + 3];
+uint32_t FileBuffer::getUint32(const size_t offset)
+{
+    uint32_t fValue = (memoryFile[offset] << 24 |
+		       memoryFile[offset + 1] << 16 |
+		       memoryFile[offset + 2] << 8) |
+		      memoryFile[offset + 3];
 
-	if (bigEndian) {
-		return fValue;
-		}
-	else {
-		__builtin_bswap32(fValue);
-		// return _byteswap_ulong(fValue);
-		}
-	}
+    if (bigEndian)
+    {
+	return fValue;
+    }
+    else
+    {
+	__builtin_bswap32(fValue);
+	// return _byteswap_ulong(fValue);
+    }
+}
 
-unsigned char FileBuffer::getUChar(const size_t offset) {
-	return 0;
-	}
-
+unsigned char FileBuffer::getUChar(const size_t offset)
+{
+    return 0;
+}
--- RawToolsTest/RawToolsTest.cpp
+++ RawToolsTest/RawToolsTest.cpp
@@ -3,10 +3,15 @@
 #include <iostream>
 #include <memory>
 
-int main() {
+int main(int argc, char* argv[]) {
   std::cout << "started test" << std::endl;
+  if (argv[1] == "") {
+    std::cout << "no file provided" << std::endl;
+    return -1;
+  }
+  std::cout << "supplied file : " << argv[1] << std::endl;
   RawTools* rt = new RawTools();
-  rt->processBlock->filename.assign("E:\\projects\\cpp\\rawTools\\sony1.ARW");
+  rt->processBlock->filename.assign(argv[1]);
   rt->getMetaData();
   rt->getBeyer();
-}
+  }
